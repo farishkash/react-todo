cee4aca5fbeebda236646e60321b08c2643ca5c7
Farish <farishkash@hotmail.com>
2017-03-08 06:51:16 -0800

    Add auth reducer and actions

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -71,22 +71,29 @@ export var startToggleTodo = (id, completed) => {
         });
     };
 };
-
-export var startLogin = ()=> {
-  return (dispatch, getState)=> {
-    firebase.auth().signInWithPopup(githubProvider).then((result)=> {
-      console.log('Auth worked! s', result)
-    }, (error)=> {
-      console.log('Unable to auth ', error)
-
-    });
-  };
+export var login = (uid) => {
+    return {type: 'LOGIN', uid};
 };
 
-export var startLogout = ()=> {
-  return (dispatch, getState)=> {
-return firebase.auth().signOut().then(()=> {
-  console.log('Logged out! ')
-});
-  };
+export var startLogin = () => {
+    return (dispatch, getState) => {
+        firebase.auth().signInWithPopup(githubProvider).then((result) => {
+            console.log('Auth worked! s', result)
+        }, (error) => {
+            console.log('Unable to auth ', error)
+
+        });
+    };
+};
+
+export var logout = () => {
+    return {type: 'LOGOUT'};
+};
+
+export var startLogout = () => {
+    return (dispatch, getState) => {
+        return firebase.auth().signOut().then(() => {
+            console.log('Logged out! ')
+        });
+    };
 };
